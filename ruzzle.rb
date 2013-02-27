10f6d1f75521f99aa996906484e32826d29d2185
brianstanwyck <brian@highgroove.com>
2013-02-27 18:26:58 -0500

    Sorts by scrabble score

--- ruzzle.rb
+++ ruzzle.rb
@@ -97,6 +97,40 @@ else
   end
 end
 
+SCRABBLE_SCORES = {
+  'A' => 1,
+  'B' => 3,
+  'C' => 3,
+  'D' => 2,
+  'E' => 1,
+  'F' => 4,
+  'G' => 2,
+  'H' => 4,
+  'I' => 1,
+  'J' => 8,
+  'K' => 5,
+  'L' => 1,
+  'M' => 3,
+  'N' => 1,
+  'O' => 1,
+  'P' => 3,
+  'Q' => 10,
+  'R' => 1,
+  'S' => 1,
+  'T' => 1,
+  'U' => 1,
+  'V' => 4,
+  'W' => 4,
+  'X' => 8,
+  'Y' => 4,
+  'Z' => 10
+}
+
+class String
+  def scrabble_score
+    upcase.split('').map { |letter| SCRABBLE_SCORES[letter] }.reduce(:+)
+  end
+end
 
 puzzle_rows = []
 
@@ -107,4 +141,4 @@ end
 
 puzzle = Puzzle.new(puzzle_rows)
 
-puts puzzle_walk(puzzle, tree).to_a.sort_by(&:length)
+puts puzzle_walk(puzzle, tree).sort_by(&:scrabble_score)
